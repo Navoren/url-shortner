0de11a5c6680bb2a006bae5d6e84137be5d68373
navoren <namantomar1453@gmail.com>
2023-12-31 22:22:14 +0530

    Update URL controller and index.js, also added views

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const { connectToMongoDB } = require('./connect')
-const URL = require('./models/url')
+const express = require('express');
+const path = require('path');
+const { connectToMongoDB } = require('./connect');
+const URL = require('./models/url');
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter")
 
 const app = express();
 const PORT = 8500;
@@ -9,22 +11,28 @@ const PORT = 8500;
 connectToMongoDB('mongodb://localhost:27017/short-url')
     .then(() => console.log('Mongodb connected...'))
 
-app.use(express.json())
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
 
-app.use("/url", urlRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/:shortId', async (req, res) => {
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId,
     },
-        {
+    {
         $push: {
             visitHistory: {
                 timestamp: Date.now()
             },
-    }
-    })
-    res.redirect(entry.redirectURL)
-})
-app.listen(PORT, ()=> console.log(`Server Listening at ${PORT}...`))
+        }
+    });
+    res.redirect(entry.redirectURL);
+});
+
+app.use("/url", urlRoute);
+app.use("/", staticRoute);
+
+app.listen(PORT, () => console.log(`Server Listening at ${PORT}...`));
